5b1398a8d053eaf3551db97b4707541534e6e60a
ivanjavierparra <ivanjavierparra@hotmail.com>
2018-06-10 00:03:29 -0300

    ejemplo grafico de torta

--- meanjs-mean-9ecbbfa/modules/atenciones/client/controllers/atenciones.client.controller.js
+++ meanjs-mean-9ecbbfa/modules/atenciones/client/controllers/atenciones.client.controller.js
@@ -65,8 +65,25 @@
             $scope.datos1= datos[0].problema;
             $scope.prob = resultado;
 
-            $scope.labels = ["Sleeping", "Designing", "Coding", "Cycling"];
-            $scope.data = [20, 40, 5, 35];
+            $scope.labels = ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"];
+            $scope.data = [12, 19, 3, 5, 2, 3];
+            $scope.backgroundColor = [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+          ];
+            $scope.borderColor =  [
+              'rgba(255,99,132,1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+          ] ;
+            $scope.borderWidth = 1;
             $scope.options = {
               elements: {
                 arc: {
